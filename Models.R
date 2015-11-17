7705600774f2ec471d0d93bb69e5bbeaf69e5add
ppaquay <p.paquay@linuxaddict.be>
2015-11-17 22:35:46 +0100

    Implementation of learning curves

--- Models.R
+++ Models.R
@@ -2,34 +2,48 @@
 
 
 library(caret)
+library(ggplot2)
 
 
-## Logloss metric function
+## Learning curve
 
 
-loglossSummary <- function(data, lev = NULL, model = NULL)  
-{
-  #print(paste(class(data$pred))) # factor  
-  data$pred <- as.numeric(data$pred) - 1 # otherwise I get an error as these are factors  
-  data$obs <- as.numeric(data$obs) - 1 # otherwise I get an error as these are factors  
-  epsilon <- .000000000000001  
-  yhat <- pmin(pmax(data$pred, rep(epsilon)), 1 - rep(epsilon))  
-  logloss <- -mean(data$obs * log(yhat) + (1 - data$obs) * log(1 - yhat))  
-  names(logloss) <- "LOGLOSS"  
-  logloss
+LogLoss <- function(obs, pred) {
+  obs <- as.numeric(obs) - 1
+  eps <- 1e-15
+  probs <- pred
+  probs[probs > 1 - eps] <- 1 - eps
+  probs[probs < eps] <- eps
+  -mean(obs * log(probs) + (1 - obs) * log(1 - probs))
 }
 
-LogLossSummary <- function (data, lev = NULL, model = NULL) 
-{
-  probs <- pmax(pmin(as.numeric(data$T), 1 - 1e-15), 1e-15)
-  logPreds <- log(probs)        
-  log1Preds <- log(1 - probs)
-  real <- (as.numeric(data$obs) - 1)
-  out <- c(mean(real * logPreds + (1 - real) * log1Preds)) * -1
-  names(out) <- c("LogLoss")
-  out
+run_learning_curve <- function(model_formula, data, y, vss = 5000, num_tss = 30, min_tss = 1000){
+  library(data.table)
+  max_tss <- nrow(data) - vss
+  tss_vector <- seq(min_tss, max_tss, length = num_tss)
+  data.table::rbindlist( lapply (tss_vector, function(tss){
+    vs_idx <- sample(1:nrow(data), vss)
+    vs <- data[vs_idx,]
+    
+    ts_eligible <- setdiff(1:nrow(data), vs_idx)
+    ts <- data[sample(ts_eligible, tss), ]
+
+    fit <- glm(model_formula, ts, family = "binomial")
+    
+    preds.ts <- rep(0, nrow(ts))
+    preds.ts[predict(fit, ts, type = "response") > 0.5] <- 1
+    preds.vs <- rep(0, nrow(vs))
+    preds.vs[predict(fit, vs, type = "response") > 0.5] <- 1
+    
+    training_error <- LogLoss(ts[, y], preds.ts)
+    validation_error <- LogLoss(vs[, y], preds.vs)
+
+    data.frame(tss = tss, 
+               error_type = factor(c("training", "validation"), 
+                                   levels = c("validation", "training")),
+               error = c(training_error, validation_error))
+  }) )
 }
-# Will use caret built-in function mnLogLoss
 
 
 ## Model 1 : Logistic regression
@@ -42,7 +56,8 @@ Logit.ctrl <- trainControl(
   summaryFunction = mnLogLoss)
 
 set.seed(1455)
-Logit.fit <- train(Made.Donation.in.March.2007 ~ Months.since.Last.Donation + Number.of.Donations + Months.since.First.Donation, data = Blood.Train, method = "glm", metric = "logLoss", family = "binomial", trControl = Logit.ctrl)
+formula <- Made.Donation.in.March.2007 ~ Months.since.Last.Donation + Number.of.Donations + Months.since.First.Donation
+Logit.fit <- train(formula, data = Blood.Train, method = "glm", metric = "logLoss", family = "binomial", trControl = Logit.ctrl)
 
 summary(Logit.fit)
 print(Logit.fit)
@@ -53,3 +68,7 @@ Submission.Logit1 <- data.frame(X = Blood.Test$X, Made.Donation.in.March.2007 = 
 colnames(Submission.Logit1) <- c("", "Made Donation in March 2007")
 write.csv(Submission.Logit1, "Submission1.csv", row.names = FALSE)
 # Leaderboard Logloss = 0.4457
+
+learning.curves.model1 <- run_learning_curve(formula, Blood.Train, 6, 114, 30, 10)
+ggplot(learning.curves.model1, aes(x = tss, y = error, linetype = error_type)) + geom_line(size = 1, col="blue") + xlab("training set size") + geom_hline(y = 10, linetype = 3)
+# The sample size is too small for the learning curves
